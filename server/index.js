ee8d6bc8b58eb6ff02d9f6d0cace396c652bb87a
Alwi Rifa <alwirifa611@gmail.com>
2023-11-30 19:46:10 +0800

    first commit

--- server/index.js
+++ server/index.js
@@ -7,13 +7,17 @@ import produksRoute from "./routes/produksRoutes.js";
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "https://pijarcamp-level3-tugas10-alwi.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://pijarcamp-level3-tugas10-alwi.vercel.app/",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+// Allow requests from 'https://pijarcamp-level3-tugas10-alwi.vercel.app'
+app.use(cors({ origin: 'https://pijarcamp-level3-tugas10-alwi.vercel.app' }));
+
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" });
