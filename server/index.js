f56517bd02c9c2edadfd3a3ac65e0a7021d8659e
Alwi Rifa <alwirifa611@gmail.com>
2023-11-30 19:42:26 +0800

    first commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "pijarcamp-level3-tugas10-alwi.vercel.app",
+    origin: "https://pijarcamp-level3-tugas10-alwi.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
